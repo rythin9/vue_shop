1e9b20ec5d87428ff4017a93afcf5422a20a1d89
rythin9 <1134746258@qq.com>
2020-03-19 03:39:27 +0800

    完成主页大致布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
-  <el-container>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
     <el-header>
       Header
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
+    <!-- 页面主体区域 -->
     <el-container>
+      <!-- 页面侧边栏 -->
       <el-aside width="200px">Aside</el-aside>
+
+      <!-- 页面内容主体 -->
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -23,4 +28,19 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container{
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,12 +1,16 @@
 // 导入element组件区域，写法如下
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message,Container,Header,Aside,Main } from 'element-ui'
 
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 
 // message导入方式不同与上面
 Vue.prototype.$message = Message
